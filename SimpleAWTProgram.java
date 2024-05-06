7600223a8276e1544ee4fb5bca6c13176df0fbe9
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-05-06 20:29:42 +0545

    Add files via upload

---
+++ SimpleAWTProgram.java
@@ -0,0 +1,38 @@
+
+// import java.awt.*;
+
+// //frame/size/label/add /setvisible true;
+// public class SimpleAWTProgram {
+//     public static void main(String[] args) {
+//         frame.add(label);
+//         Frame frame = new Frame("Hello BCA");
+//         frame lable = new label("Hello AWT");
+//         frame.setSize(400,400);
+//         frame.setVisible(true); 
+        
+//     }
+// }
+//event-handler
+//frame
+//panel
+//label-helps to make clean text
+//button
+//textfield
+//chocie
+//option
+//list choice
+
+import java.awt.*;
+
+public class SimpleAWTProgram {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame("Hello BCA");
+        Label label = new Label("Hello AWT");
+        frame.add(label);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+
+}
+
