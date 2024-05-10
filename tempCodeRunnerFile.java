56d493f8b3f2876a8bb426e275ee807f78671117
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-05-10 10:47:35 +0545

    Add files via upload

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+        // if (e.getSource() == btn1) {
+        //     // Code to handle btn1 click
+        //     System.out.println("Yes button clicked");
+        // } else if (e.getSource() == btn2) {
+        //     // Code to handle btn2 click
+        //     System.out.println("No button clicked");
+        // }
